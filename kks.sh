d785e18e822473914edde3d8d401e58d3a551ac1
tom <liyuanjunmail@qq.com>
2017-06-24 14:48:17 +0800

    .

--- kks.sh
+++ kks.sh
@@ -3,4 +3,4 @@
 curl -ssL https://raw.githubusercontent.com/amlyj/remote-scripts/master/linux/kill_service > kks&&\
 chmod +x ./kks&&\
 sudo mv ./kks /usr/local/bin/&&\
-echo '\ninstall success...'
+echo -e '""\n   install success...\n   "please run kks" kill your service...\n'
