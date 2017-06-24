0c78b531189ea322d1f55f65d3f59aad96bc882f
tom <liyuanjunmail@qq.com>
2017-06-24 14:46:02 +0800

    Revert "."
    This reverts commit 6d25305b84af9f919263a332f9e933fc07328161.

--- kks.sh
+++ kks.sh
@@ -3,4 +3,4 @@
 curl -ssL https://raw.githubusercontent.com/amlyj/remote-scripts/master/linux/kill_service > kks&&\
 chmod +x ./kks&&\
 sudo mv ./kks /usr/local/bin/&&\
-echo -e '""\ninstall success...\n"please run kks" kill your service'
+echo '\ninstall success...'
